a260f6efd01209d7019b9277c4f8ca07641ace51
Simon Skoczylas <simon.skoczylas@materna.de>
2017-10-18 08:13:27 +0200

    Addes exercise distribution

---
+++ 7-react-distribution/result/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'Hello from the distribution';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('root')
+);
